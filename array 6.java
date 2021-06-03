41b7ed7aee8cc957ac2867304076a762f1558893
ankit-kumar-singh517 <68545337+ankit-kumar-singh517@users.noreply.github.com>
2021-06-03 23:19:18 +0530

    Add files via upload

---
+++ array 6.java
@@ -0,0 +1,23 @@
+//arra ---how we can represent the array in the program and by using the how many .
+public class ArrayEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+		int num[]=new int[5];
+		
+		num[0]=20;
+		num[1]=20;
+		num[2]=30;
+		num[3]=40;
+		num[4]=50;
+		for( int n:num)
+		{
+			System.out.println(n);
+			
+		}
+		
+		
+	}
+
+}
