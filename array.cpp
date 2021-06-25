654747388022b420772233cc437e3d4558141a56
ankit-kumar-singh517 <68545337+ankit-kumar-singh517@users.noreply.github.com>
2021-06-26 00:01:32 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,33 @@
+
+
+===================Array example======================
+
+
+import java.util.*;
+public class ArrayEx3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num[]=new int[5];
+		int data[]= {78,89,45,56,12};
+		//Scanner input=new Scanner(System.in);
+		System.out.println("number of array are=");
+		 for(int n:data)
+		 {
+			 System.out.println(n);
+		 }
+	}
+
+}
+
+
+
+Solution ============================
+
+number of array are=
+78
+89
+45
+56
+12
